bb2ee3cd77389dabefeb25aeca0a3b6783097b95
EyadJawad <ayadmansr@gmail.com>
2026-02-15 11:35:43 +0300

    Delete old unused methods from struct encodedChars

--- include/types/encodedchars.h
+++ include/types/encodedchars.h
@@ -4,16 +4,4 @@
 struct encodedChars {
     uint32_t n;
     uint8_t len;
-
-    uint64_t getId () {
-        
-        return (static_cast <uint64_t> (len) << 32 |
-                static_cast <uint64_t> (n));
-    }
-    bool operator() (
-        const int &Len1,
-        const int &Len2
-    )   const {
-        return Len1 > Len2;
-    }
 };
